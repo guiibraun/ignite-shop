201989b9919c6bfb108d91548e7ffe24f74edd2e
Guilherme Braun <guiabraun@hotmail.com>
2022-10-26 09:47:30 -0300

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 import logoImg from '../assets/logo.svg'
-import { Container, Header } from '../styles/pages/app'
+import { Container, Header, MiniCart } from '../styles/pages/app'
 import Image from 'next/future/image'
 import Link from 'next/link'
+import { Handbag } from 'phosphor-react'
+import { CartProvider } from 'use-shopping-cart'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Link href="/"><Image src={logoImg} alt='' /></Link>
-      </Header>
-      <Component {...pageProps} />
-    </Container>
+    <CartProvider mode="payment" cartMode='client-only' stripe={process.env.STRIPE_PUBLIC_KEY} currency="BRL" successUrl={process.env.NEXT_URL} cancelUrl={process.env.NEXT_URL}>
+      <Container>
+        <Header>
+          <Link href="/"><Image src={logoImg} alt='' /></Link>
+          <MiniCart>
+            <Handbag weight='bold' size={23}/>
+          </MiniCart>
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </CartProvider>
   )
 }
